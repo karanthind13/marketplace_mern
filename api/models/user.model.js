be3b6696caf81aea8f949e53a1e22092a88039d6
Karanpal Singh <karanpalthind990@gmail.com>
2025-03-31 01:35:05 -0400

    create and add OAuth component

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -14,7 +14,11 @@ const userSchema = new mongoose.Schema({
     password:{
         type:String,
         required:true,
-    }
+    },
+    avatar:{
+        type:String,
+        default:"https://static.vecteezy.com/system/resources/previews/009/734/564/non_2x/default-avatar-profile-icon-of-social-media-user-vector.jpg"
+    },
 
 },{timestamps:true});
 
