931d15df6321764a0300d0d4866f2238e784f2c1
Karanpal Singh <karanpalthind990@gmail.com>
2025-04-09 10:12:53 -0400

    image slider to listing page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -13,7 +13,7 @@ const Profile = () => {
   const [filePerc, setFilePerc]=useState(0)
   const [fileUploadError, setFileUploadError]= useState(false);
   const [formData, setFormData]= useState({});
-  const [upadateSuccess, setUpdateSuccess]= useState(false);
+  const [updateSuccess, setUpdateSuccess]= useState(false);
   const [showListingsError, setShowListingsError]= useState(false);
   const [userListings, setUserListings]= useState([])
   const dispatch = useDispatch(); 
@@ -113,7 +113,7 @@ const Profile = () => {
     } catch (error) {
       dispatch(signOutUserFailure(data.message))
     }
-  }
+  }                                                                                                                                                                                                 
 
   const handleShowListings = async()=>{
     try {
@@ -180,7 +180,7 @@ const Profile = () => {
       </div>
 
       <p className="text-red-700 mt-5">{error ? error :""}</p>
-      <p className="text-green-700">{upadateSuccess ? 'User is updated successfully': ''}</p>
+      <p className="text-green-700">{updateSuccess ? 'User is updated successfully': ''}</p>
       <button onClick={handleShowListings} className="text-green-700 w-full">Show Listings</button>
       <p className="text-red-700 mt-5">{showListingsError ? 'Error showing listings ':''}</p>
 
@@ -189,10 +189,10 @@ const Profile = () => {
         <h1 className="text-center mt-7 text-2xl font-semibold">Your Listings</h1>
            {userListings.map((listing)=> 
       <div key={listing._id} className="border rounded-lg p-3 flex justify-between items-center gap-4">
-        <Link to={`'/listing/${listing._id}`}>
+        <Link to={`/listing/${listing._id}`}>
           <img src={listing.imageUrls[0]} alt="listing cover" className="h-16 w-16 object-contain"/>
         </Link>
-        <Link className="text-slate-700 font-semibold  hover:underline truncate flex-1" to={`'/listing/${listing._id}`}>
+        <Link className="text-slate-700 font-semibold  hover:underline truncate flex-1" to={`/listing/${listing._id}`}>
           <p>{listing.name}</p>
         </Link>
 
